cb26ef4e224fb4c893f59323e24befb96ee93efa
rdanf1 <reylet.daniel@gmail.com>
2023-03-22 19:37:07 +0100

    C'est pas du chinois. (almost there)

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-# 실행경로가 다르더라도 정확한 주소를 받기 위해 사용
 # Use to get the correct address even if the execution path is different
 main_dir = os.path.dirname(__file__)
 
 app = FastAPI()
 
-# 템플릿과 스태틱 파일 가져오기
 # Import templates and static files
 # Original structure
 # templates = Jinja2Templates(directory=f"{main_dir}/templates")
