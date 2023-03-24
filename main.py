c6852107dc72d6e539065099143d5b1be151b3e2
rdanf1 <reylet.daniel@gmail.com>
2023-03-24 16:29:03 +0100

    Good Practices ( Say so : Fixing Problems )

--- main.py
+++ main.py
@@ -23,14 +23,15 @@ app = FastAPI()
 templates = Jinja2Templates(directory=f"{main_dir}/src")
 app.mount(
     "/static", StaticFiles(directory=f"{main_dir}/static"), name="static")
-
+'''
 # Original structure
 #@app.get("/")
 #async def test(request: Request):
 #    return templates.TemplateResponse("test.html.ori", {"request": request, "data": "Test"})
 # DR - Migration's
-my_list = ['/','/src/','/src/index.html']
-#my_list = ["/"]
+'''
+my_list = ['/', '/src/', '/src/index.html']
+# my_list = ["/"]
 for text in my_list:
     @app.get(text)
     async def test(request: Request):
@@ -49,50 +50,61 @@ vvvv NOT SURE IT RETURN A TEMPLATE (ERROR UTF-8 text/html read...)"""
 async def img_dan( request: Request ):
     return templates.TemplateResponse("/static/img_dan-3.webp", {"request": request, "data": "Test"})
 '''
+
+
 @app.get("/src/page2.html")
 async def page2(request: Request):
     return templates.TemplateResponse("page2.html", {"request": request, "data": "Test"})
+
 
 @app.get("/src/page3-B-Cards.html")
 async def page3(request: Request):
     return templates.TemplateResponse("page3-B-Cards.html", {"request": request, "data": "Test"})
 
+
 @app.get("/src/php/page3.php")
 async def page3_php(request: Request):
     return templates.TemplateResponse("php/page3.php", {"request": request, "data": "Test"})
 
+
 @app.get("/src/php/msgFile.html")
-async def msgFile(request: Request):
+async def msg_file(request: Request):
     return templates.TemplateResponse("php/msgFile.html", {"request": request, "data": "Test"})
 
+'''
 @app.get("/src/php/like.php")
 async def like(request: Request):
     return templates.TemplateResponse("php/like.php", {"request": request, "data": "Test"})
+'''
+
 
 @app.post("/src/php/like.php")
-async def likePost(request: Request):
+async def like_post(request: Request):
     return templates.TemplateResponse("php/like.php", {"request": request, "data": "Test"})
 
+
 @app.get("/src/php/note_form.php")
-async def noteForm(request: Request):
+async def note_form(request: Request):
     return templates.TemplateResponse("php/note_form.php", {"request": request, "data": "Test"})
 
+
 @app.post("/src/php/add_note.php")
-async def addNote(request: Request):
+async def add_note(request: Request):
     return templates.TemplateResponse("php/add_note.php", {"request": request, "data": "Test"})
 
+
 @app.get("/src/php/class/TestClass.php")
-async def testPOO(request: Request):
+async def test_poo(request: Request):
     return templates.TemplateResponse("class/TestClass.php", {"request": request, "data": "Test"})
 
 
 # read as text/html => error
-#@app.get("/src/js/script.js")
-#async def test(request: Request):
+# @app.get("/src/js/script.js")
+# async def test(request: Request):
 #    return templates.TemplateResponse("js/script.js")
 
 ''' DR - Was part of initial project 
-     ( much proper - Iconfess I tainted it ALL ) 
+     ( much proper - I confess I tainted it ALL ) 
 @app.post("/result")
 async def test(idx: int = Form()):
     return {"idx": idx}
